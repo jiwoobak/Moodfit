0bffd1bbe5484f3cce6f3c6f804a19a1e2fe9219
jaehe0130 <165889783+jaehe0130@users.noreply.github.com>
2025-11-23 17:10:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st
+import time
 
 st.set_page_config(page_title="MoodFit", page_icon="🏋️", layout="centered")
 
-st.title("🏋️ MoodFit - 당신의 감정 기반 운동 추천 서비스")
+st.markdown("""
+    <h1 style='text-align:center; font-size:40px;'>🏋️ MoodFit</h1>
+    <p style='text-align:center; font-size:20px; color:gray;'>당신의 감정 기반 운동 추천 서비스</p>
+""", unsafe_allow_html=True)
 
-st.switch_page("pages/1_daily_info2.py")
+# 3~4초 대기 후 자동 이동
+time.sleep(3)  # ← 3초 보여줌
+st.switch_page("pages/1_user_info2.py")
